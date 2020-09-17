789d9c2610429b7f485a326084a8ef4fce255627
Yao-Yin <Yao-Yin1997@users.noreply.github.com>
2020-09-17 09:19:02 +0900

    cache lab setup

---
+++ target1/lv3.d
@@ -0,0 +1,11 @@
+
+lv3.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 bf 10 c4 fd ff ff 	movabs $0x7ffffffdc410,%rdi
+   7:	7f 00 00 
+   a:	68 ec 17 40 00       	pushq  $0x4017ec
+   f:	c3                   	retq   
---
+++ target1/lv3.s
@@ -0,0 +1,3 @@
+mov $0x7ffffffdc410, %rdi
+pushq $0x4017ec
+retq
